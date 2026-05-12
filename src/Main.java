4b877d0c28601d00020a821d19b4b84c5956c673
Petr Poroykov <pporoykov@gmail.com>
2026-05-12 12:32:58 +0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
+import java.util.AbstractQueue;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<String> firstClients = List.of("Anya", "Sveta", "Olya", "Alexandra", "Ruslana", "Olesya", "Vika");
+
+        Queue<String> customerQueue = new LinkedList<>();
+
+        customerQueue.addAll(firstClients);
+
+//            customerQueue.offer("Petya");
+//            customerQueue.offer("Olya");
+//            customerQueue.offer("Tanya");
+
+//            customerQueue.poll();
+
+        while (!customerQueue.isEmpty()) {
+            String currentСlient = customerQueue.poll();
+            System.out.println(currentСlient + " сделал новый маникюр");
+            if (Math.random() < 0.5) {
+// друг записался
+                customerQueue.offer("a friend of " + currentСlient);
+                System.out.println("Плюс новый клиент!!!");
+//            } else {
+// друг не записался
+            }
+            System.out.println(customerQueue);
+        }
+    }
 }
